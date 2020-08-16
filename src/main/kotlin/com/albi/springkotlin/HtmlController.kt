99413ebf2a9a2b0c9bac8f01d6e306e2ca00a6eb
albihasani94 <albihasani94@gmail.com>
2020-08-16 03:38:49 +0200

    Add properties

--- src/main/kotlin/com/albi/springkotlin/HtmlController.kt
+++ src/main/kotlin/com/albi/springkotlin/HtmlController.kt
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.server.ResponseStatusException
 
 @Controller
-class HtmlController(private val repository: ArticleRepository) {
+class HtmlController(private val repository: ArticleRepository, private val properties: BlogProperties) {
 
     @GetMapping
     fun blog(model: Model): String {
-        model["title"] = "Blog"
+        model["title"] = properties.title
+        model["banner"] = properties.banner
         model["articles"] = repository.findAllByOrderByAddedAtDesc().map { it.render() }
         return "blog"
     }
--- src/main/kotlin/com/albi/springkotlin/SpringKotlinApplication.kt
+++ src/main/kotlin/com/albi/springkotlin/SpringKotlinApplication.kt
@@ -1,11 +1,13 @@
 package com.albi.springkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
 class SpringKotlinApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringKotlinApplication>(*args)
+    runApplication<SpringKotlinApplication>(*args)
 }
